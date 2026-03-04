e5a32b9404a39a8d0a21aa43b8e5824e947f1c9c
julixnj31 <julixnj31@gmail.com>
2026-03-04 10:51:06 -0500

    Revert "Release"

--- 1.js
+++
@@ -1,5 +0,0 @@
-let producto = prompt("Ingrese el nombre del producto:");
-let precio = parseFloat(prompt("Ingrese el precio del producto:"));
-let cantidad = parseInt(prompt("Ingrese la cantidad comprada:")); 
-let total = precio * cantidad;
-console.log("El total a pagar por " + cantidad + " " + producto + " es: " + total);
--- 2.js
+++
@@ -1,4 +0,0 @@
-let horas = parseFloat(prompt("Ingrese el número de horas trabajadas:")); 
-let valorHora = parseFloat(prompt("Ingrese el valor por hora:"));
-let salario = horas * valorHora;
-console.log("El salario semanal es: " + salario);
